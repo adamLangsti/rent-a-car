a2c5ce0deabab418664d6432a0960583e6e69889
Adam Lang <adamjoshualang@gmail.com>
2023-08-01 19:55:19 +0300

    feat: boolean function for menu open. dynamic classname based on state of menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen((prevMenu) => !prevMenu);
+    };
+
     return (
-        <div className='navbar-container'>
-            <ul className='navbar-items'>
+        <nav className='navbar-container'>
+            <FontAwesomeIcon
+                icon={menuOpen ? faTimes : faBars}
+                className={`menu-icon ${menuOpen ? 'open' : ''}`}
+                onClick={toggleMenu}
+            />
+            <ul className={`navbar-items ${menuOpen ? 'active' : 'inactive'}`}>
                 <li className='navbar-list-items'>
                     <a href='#' className='navbar-list-items-links'>
                         Start
@@ -31,7 +44,7 @@ const Navbar = () => {
                     </a>
                 </li>
             </ul>
-        </div>
+        </nav>
     );
 };
 
