9847473cccefc696810a83f09907de5e2302d5a5
Adam Lang <adamjoshualang@gmail.com>
2023-08-02 20:44:27 +0300

    feat: added car images in hero section, carousel spins images using setInterval

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,31 @@
-import React from "react"; 
+import React, { useEffect, useState } from 'react';
+import '../styles/Hero.css';
+import logo1 from '../images/volvo.png';
+import logo2 from '../images/merc.png';
+import logo3 from '../images/rolls.png';
 
 const Hero = () => {
-    return <div></div>;
+    const images = [logo1, logo2, logo3];
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [images.length]);
+
+    return (
+        <section className='hero-container'>
+            <div className='overlay' />
+            <img
+                src={images[activeIndex]}
+                alt='car-selection'
+                style={{ maxWidth: '40%' }}
+            />
+        </section>
+    );
 };
 
 export default Hero;
